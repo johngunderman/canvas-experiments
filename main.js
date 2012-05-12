b1e89ccff3e3a5f5fbc364a0b9dd92a9af9afec8
John Gunderman <johngunderman@gmail.com>
2012-05-12 09:22:40 -0700

    Our nodes draw now.
    This will require some modification, as the current implementation
    creates a new shape object for each node each time we call the draw
    function. An alternative would be to have nodes draw themselves as
    part of an initialization routine, and then use the .move() method
    for any further modifications.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+var NODE_RADIUS = 5;
+var NODE_COLOR = "red";
+var NODE_STROKE_COLOR = "black";
+var NODE_STROKE_WIDTH = 2
+
+var EDGE_STROKE_COLOR = "black";
+var EDGE_STROKE_WIDTH = 3;
+
+
 var model = {}
 
 window.onload = function() {
@@ -9,15 +18,17 @@ window.onload = function() {
     model.addNode();
 
     model.simulate();
+    model.draw();
 };
 
 function Model() {
     this.stage = new Kinetic.Stage({
         container: "board",
-        width: 500,
-        height: 600
+        width: 1000,
+        height: 500
     });
     this.layer = new Kinetic.Layer();
+    this.stage.add(this.layer);
     this.nodes = [];
     this.edges = [];
 }
@@ -25,12 +36,12 @@ function Model() {
 Model.prototype = {
 
     addNode: function() {
-        var node = new Node();
+        var node = new Node(this);
         this.nodes.push(node);
     },
 
     addEdge: function(node1, node2) {
-        var edge = new Edge(node1, node2);
+        var edge = new Edge(this, node1, node2);
         this.edges.push(edge);
     },
 
@@ -39,15 +50,26 @@ Model.prototype = {
     },
 
     draw: function() {
-        stage.draw();
+        for (e in this.edges) {
+            this.edges[e].draw();
+        }
+
+        for (n in this.nodes) {
+            this.nodes[n].draw();
+        }
+        this.stage.draw();
     }
 };
 
 
-function Node() {
+function Node(model) {
+    this.model = model
     this.neighbors = [];
-    this.x = 0;                 // set randomly during simulation
-    this.y = 0;                 // set randomly during simulation
+    // set random x,y coords. small chance of error if we get the same
+    // coords for multiple nodes.
+    this.x = Math.floor(Math.random() * this.model.stage.width);
+    this.y = Math.floor(Math.random() * this.model.stage.height);
+    this.shape = {}             // stores the kinetic.js shape
 }
 
 Node.prototype = {
@@ -59,13 +81,48 @@ Node.prototype = {
         else {
             this.neighbors.push(node);
         }
+    },
+
+    draw: function() {
+        this.shape = new Kinetic.Circle({
+            x: this.x,
+            y: this.y,
+            radius: NODE_RADIUS,
+            fill: NODE_COLOR,
+            stroke: NODE_STROKE_COLOR,
+            strokeWidth: NODE_STROKE_WIDTH
+        });
+
+        this.model.layer.add(this.shape);
     }
 };
 
-function Edge(node1, node2) {
+function Edge(model, node1, node2) {
+    this.model = model;
     this.node1 = node1;
     this.node2 = node2;
 
     this.node1.addNeighbor(node2);
     this.node2.addNeighbor(node1);
 }
+
+Edge.prototype = {
+
+    draw: function() {
+        var points = [{
+            x: this.node1.x,
+            y: this.node1.y
+        }, {
+            x: this.node2.x,
+            y: this.node2.y
+        }];
+
+        this.line = new Kinetic.Line({
+            points: points,
+            stroke: EDGE_STROKE_COLOR,
+            strokeWidth: EDGE_STROKE_WIDTH
+        });
+
+        this.layer.add(this.line);
+    }
+};
